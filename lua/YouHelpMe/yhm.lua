41226d4bda1c7143535f278c48d30938f04b0b1a
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-03-28 05:28:57 +0800

    update YHM

--- lua/YouHelpMe/yhm.lua
+++ lua/YouHelpMe/yhm.lua
@@ -10,6 +10,9 @@ local json = require "json"
 
 --> API stuff <--
 
+--- @class YHM
+--- @field private _config_table table
+--- @field _version string Version of YHM (in semver)
 YHM = { _version = "0.1", _config_table = {} }
 
 local f = io.open("sys/yhm-config.json",'r')
@@ -19,8 +22,14 @@ if f then
     f:close()
 end
 
+--[[
+Get messages from team, return an iterator function.
+]]
+--- @param team_id integer
+--- @return function
+--- @see YHM.get_all_messages
 function YHM:get_messages_from_team(team_id)
-    if team_id < 0 or team_id > 3 then
+    if team_id < 1 or team_id > 3 then
         error("invalid team id: "..team_id)
     end
 
@@ -32,6 +41,11 @@ function YHM:get_messages_from_team(team_id)
     end
 end
 
+--[[
+Get messages from all team (T, CT, VIP), return an iterator function.
+]]
+--- @return function
+--- @see YHM.get_messages_from_team
 function YHM:get_all_messages()
     local tmp = {}
     for _,v in pairs(self._config_table) do
@@ -54,10 +68,21 @@ function YHM:get_all_messages()
     end
 end
 
+--[[
+Overwrite the config, replaces current config if `sys/yhm-config.json` found.
+If omit `tbl`, it'll set the config as empty table.
+]]
+--- @param tbl? any
+--- @see YHM.get_config
 function YHM:overwrite_config(tbl)
     self._config_table = tbl or {}
 end
 
+--[[
+Get the current config.
+]]
+--- @return table
+--- @see YHM.overwrite_config
 function YHM:get_config()
     return self._config_table
 end
@@ -67,7 +92,9 @@ end
 addhook("spawn","YHM_spawn_hook")
 
 function YHM_spawn_hook(id)
-    local team_id = player(id, "team")
+    math.randomseed(os.time())
+
+    local team_id = player(id,"team")
     local team_config = YHM._config_table[tostring(team_id)]
 
     if team_config then
@@ -75,4 +102,3 @@ function YHM_spawn_hook(id)
         msg2(id,"\169255026026Tips: "..team_config[math.random(n)].."@C")
     end
 end
-
